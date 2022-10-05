184c6604e675fcfd3b6f6189492def70ad088671
杨静 <1369374442@qq.com>
2022-10-05 22:08:06 +0800

    feat: 使用http模块创建一个服务

--- commonjs-test/b.js
+++ commonjs-test/b.js
@@ -1,3 +1,7 @@
 const { add, name} = require('./a');
-console.log(add(10, 20));
-console.log(name);
+// console.log(add(10, 20));
+// console.log(name);
+
+const _ = require('lodash');
+const arr = _.concat([1, 2], 3);
+console.log('...arr', arr);
--- debugger-test/app.js
+++ debugger-test/app.js
@@ -1,5 +1,13 @@
-console.log(100);
-console.log(200);
-console.log(300);
-console.log(400);
-console.log(500);
+// 引入了 http 模块
+const http = require('http');
+// 使用 http 模块创建了一个 http 服务
+const server = http.createServer((req, res) => {
+  // 返回 200，内容类型为文本
+  res.writeHead(200, {'content-type': 'text/html'});
+  // 返回内容
+  res.end('<h1>hello world11</h1>');
+});
+// 监听 300 端口
+server.listen(3000, () => {
+  console.log('server is running');
+});
